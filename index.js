48f8c96bec15243019b330de94b85b0d34cb8b1e
Derek Miranda <derek.miranda.1992@gmail.com>
2019-10-09 14:31:06 -0700

    relint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import 'ad-polyfills'
-import * as scope from './lib/scope.js'
-import * as polite from './lib/polite.js'
-import * as preloader from './lib/preloader.js'
-import * as payloader from './lib/payloader.js'
+import "ad-polyfills";
+import * as scope from "./lib/scope.js";
+import * as polite from "./lib/polite.js";
+import * as preloader from "./lib/preloader.js";
+import * as payloader from "./lib/payloader.js";
 
 /*
  * 	NOTE:
@@ -12,36 +12,36 @@ import * as payloader from './lib/payloader.js'
  */
 
 polite
-	.prepare(window.adParams.politeLoadAfter)
+  .prepare(window.adParams.politeLoadAfter)
 
-	// prepare vendor onload (Network.js)
-	.then(() => window.prepareIndex())
+  // prepare vendor onload (Network.js)
+  .then(() => window.prepareIndex())
 
-	// prepare scope
-	.then(() => scope.prepare())
+  // prepare scope
+  .then(() => scope.prepare())
 
-	// misc index control
-	.then(() => window.prepareAdParamsMisc())
+  // misc index control
+  .then(() => window.prepareAdParamsMisc())
 
-	// prepare network
-	.then(() => window.prepareNetworkExit())
+  // prepare network
+  .then(() => window.prepareNetworkExit())
 
-	// prepare preloader
-	.then(() => preloader.prepare())
-	.then(() => window.preparePreloadMisc())
+  // prepare preloader
+  .then(() => preloader.prepare())
+  .then(() => window.preparePreloadMisc())
 
-	// finish polite timeout
-	.then(() => polite.resolveDelay())
+  // finish polite timeout
+  .then(() => polite.resolveDelay())
 
-	// prepare payload
-	.then(() => payloader.execute())
+  // prepare payload
+  .then(() => payloader.execute())
 
-	// launch polite
-	.then(fbaContent => {
-		preloader.isComplete.then(() => window.onImpression(fbaContent))
-	})
+  // launch polite
+  .then(fbaContent => {
+    preloader.isComplete.then(() => window.onImpression(fbaContent));
+  })
 
-	.catch(err => {
-		console.error(err)
-		window.failAd()
-	})
+  .catch(err => {
+    console.error(err);
+    window.failAd();
+  });
