c013891a85662b55769ce1961653c3f459fd6fe4
Greg Connell <gconnell@160over90.com>
2021-08-23 18:33:48 -0700

    make sure err throws do not get dropped by minification

--- index.js
+++ index.js
@@ -12,36 +12,31 @@ import * as payloader from './lib/payloader.js'
 // callback to allow control of when polite-load launches
 let preloaderComplete
 const begin = async () => {
-	try {
-		await polite.prepare(window.adParams.politeLoadAfter)
-		// prepare vendor onload (Network.js)
-		await window.prepareIndex()
-		// prepare scope
-		scope.prepare()
-		// misc index control
-		await window.prepareAdParamsMisc()
-		// prepare network
-		await window.prepareNetworkExit()
+	await polite.prepare(window.adParams.politeLoadAfter)
+	// prepare vendor onload (Network.js)
+	await window.prepareIndex()
+	// prepare scope
+	scope.prepare()
+	// misc index control
+	await window.prepareAdParamsMisc()
+	// prepare network
+	await window.prepareNetworkExit()
 
-		// prepare preloader
-		const preloaders = await preloader.loadPreloaders(window.assets)
-		// allow index custom preloader routines to resolve
-		preloaderComplete = window.preparePreloader(preloaders) || Promise.resolve()
+	// prepare preloader
+	const preloaders = await preloader.loadPreloaders(window.assets)
+	// allow index custom preloader routines to resolve
+	preloaderComplete = window.preparePreloader(preloaders) || Promise.resolve()
 
-		// finish polite timeout (some networks require delay before asset load)
-		await polite.resolveDelay()
+	// finish polite timeout (some networks require delay before asset load)
+	await polite.resolveDelay()
 
-		// load build payloads: fonts, scripts, images, binaries
-		const assets = await payloader.loadAssets(window.assets)
+	// load build payloads: fonts, scripts, images, binaries
+	const assets = await payloader.loadAssets(window.assets)
 
-		// preload complete
-		await preloaderComplete
+	// preload complete
+	await preloaderComplete
 
-		// launch polite
-		window.onImpression({ ...assets, preloaders })
-	} catch (err) {
-		console.error(err)
-		window.useBackup()
-	}
+	// launch polite
+	window.onImpression({ ...assets, preloaders })
 }
 begin()
